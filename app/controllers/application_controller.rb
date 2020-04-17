101709785b7f627b95e884a619d648ef8c099a7b
Josefine Theden-Schow <josi.theden@gmx.de>
2020-04-17 10:50:14 +0200

    cloudinary fixing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,4 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
-
-  def after_sign_in_path_for(resource)
-    books_path
-  end
-
-  def after_sign_up_path_for(scope)
-    books_path
-  end
 end
